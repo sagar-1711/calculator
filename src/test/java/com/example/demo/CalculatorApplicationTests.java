b5daa9dd315947d08f37754e54d5bd6c47fb35b3
sagar.tiwari@thalesgroup.com <Thales@2021>
2021-03-17 22:17:36 +0530

    Fifth commit

--- src/test/java/com/example/demo/CalculatorApplicationTests.java
+++ src/test/java/com/example/demo/CalculatorApplicationTests.java
@@ -1,18 +1,27 @@
 package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.demo.controller.CalculatorController;  
 @SpringBootTest
 class CalculatorApplicationTests {
 
+
 	@Test
-	void contextLoads() {
-		System.out.println("HEllo");
+	void checkAddOverflow() {
+		assertNotEquals(4294967294l,CalculatorController.divide("2147483647","2147483647"));
 	}
-//	@Test
-//	void checkOverflow() {
-//		assertEquals(1,(int)CalculatorController.divide("2","2"));
+	@Test
+	void checkAddParameters()
+	{
+		assertThrows(NumberFormatException.class, () -> {
+			String a = "100";
+			String b = "sadd";
+			CalculatorController.add(a, b);
+			});
 	}
+	
+}
 
 	
